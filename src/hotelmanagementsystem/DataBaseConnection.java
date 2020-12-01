660e459444620c4245645288486064d73efd6705
SuryaRongali <surya.rongali2@gmail.com>
2020-11-30 22:24:51 -0600

    project files update

---
+++ src/hotelmanagementsystem/DataBaseConnection.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelmanagementsystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Surya Rongali
+ */
+public class DataBaseConnection {
+    static Connection connection;
+   
+    public static Connection getConnection(){
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+	    String url = "jdbc:mysql://localhost/hotel_management_db";
+            connection=DriverManager.getConnection(url,"root","");
+            System.out.println("Connected");
+            
+        } catch (Exception e) {
+            
+        }
+        
+        return connection;
+    }
+    
+    public static void main(String[] args) {
+        getConnection();
+}
+}
